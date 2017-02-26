7b76bc009dc19bf0856de89692783cde65e0b5ba
Jeffrey Fulton <jeffrey.fulton@me.com>
2017-02-26 15:41:41 -0600

    Initial Commit

---
+++ PresentersAndViewModelsFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PresentersAndViewModelsFun
+//
+//  Created by Jeffrey Fulton on 2017-02-26.
+//  Copyright © 2017 Jeffrey Fulton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
